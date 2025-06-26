e38b419c43ed97222b7df24ba33b82d353844ba2
sri surya prakash reddy <ramram185085@gmail.com>
2025-06-26 16:03:22 +0530

    transfer -success version ui success implementing -budget update 2

--- src/main/java/com/project/finance/ExpensePlannerApplication.java
+++ src/main/java/com/project/finance/ExpensePlannerApplication.java
@@ -1,18 +1,7 @@
 package com.project.finance;
 
-
-import com.project.finance.model.Account;
-import com.project.finance.model.Budget;
-import com.project.finance.model.Transactions;
-import com.project.finance.service.AccountService;
-import com.project.finance.service.BudgetService;
-import com.project.finance.service.TransactionService;
-import com.project.finance.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class ExpensePlannerApplication {
--- src/main/java/com/project/finance/service/EmailService.java
+++ src/main/java/com/project/finance/service/EmailService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
     public void sendEmail(String to, String subject, String body) {
-        // In real app, integrate with SMTP provider
+
         System.out.println("Sending email to " + to + ": " + subject + " - " + body);
     }
 }
